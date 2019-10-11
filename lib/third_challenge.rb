3ef15e9c673c8e629f2863aa3e3fc5bd001103e9
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-11 13:31:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,17 +3,18 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-      name: "Lord Montague", age: "53"},
+      name: "Lord Montague", 
+      age: "53"
         },
       :matriarch => {
         #your key/value pairs here
         name: "Lady Montague",
-        age: 54
+        age: "54"
         },
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: 15,
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -22,17 +23,17 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         name: "Lord Capulet",
-        age: 50
+        age: "50"
         },
       :matriarch => {
         #your key/value pairs here
         name: "Lady Capulet",
-        age: 51
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
         name: "Juliet",
-        age: 15
+        age: "15"
         status: "alive"
         },
       :heroine_friends => []
